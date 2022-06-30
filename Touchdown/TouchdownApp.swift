8f9fa99a73c2091ba2f01c26273b2555f801fb19
Zach Cervi <zachcervi@gmail.com>
2022-06-30 15:52:23 -0600

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Zach Cervi on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
