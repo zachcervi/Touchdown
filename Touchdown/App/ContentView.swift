4872e64d1b8734b01e7de6369ae2540e5af40c6b
Zach Cervi <zachcervi@gmail.com>
2022-07-04 10:59:27 -0600

    added brand section

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -35,6 +35,10 @@ struct ContentView: View {
                             }//: LOOP
                         })//: GRID
                         .padding(15)
+                        
+                        TitleView(title: "Brands")
+                        BrandGridView()
+                        
                         FooterView()
                             .padding(.horizontal)
                     }//: VSTACK
---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Zach Cervi on 7/4/22.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
