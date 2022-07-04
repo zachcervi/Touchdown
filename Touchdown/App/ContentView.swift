2ab678051adf773c2531d439446a2df048435329
Zach Cervi <zachcervi@gmail.com>
2022-07-04 09:50:08 -0600

    Tab item view

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -21,9 +21,16 @@ struct ContentView: View {
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
                 
-                Spacer()
-                FooterView()
-                    .padding(.horizontal)
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0){
+                        FeaturedTabView()
+                            .frame(height: UIScreen.main.bounds.width / 1.475)
+                          .padding(.vertical, 20)
+                        FooterView()
+                            .padding(.horizontal)
+                    }//: VSTACK
+                })//: SCROLL
+             
             }//: VSTACK
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         }//: ZSTACK
@@ -34,5 +41,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro")
     }
 }
---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Zach Cervi on 7/4/22.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
